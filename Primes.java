dfa7f47987e9c7c557d28f31b5fcb38ee026e096
Merav-M <merav.mauhnoom@post.runi.ac.il>
2024-12-05 23:03:23 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,40 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int nInputNum = Integer.parseInt(args[0]);
+
+        boolean[] isPrime = new boolean[nInputNum + 1];
+
+        for (int i = 2; i <= nInputNum; i++) {
+            isPrime[i] = true;
+        }
+
+        int nPrime = 2;
+        while (nPrime * nPrime <= nInputNum) {
+            if (isPrime[nPrime]) {
+                // Mark multiples of p as non-prime
+                int nMultipleOfPrime = nPrime * nPrime;
+                while (nMultipleOfPrime <= nInputNum) {
+                    isPrime[nMultipleOfPrime] = false;
+                    nMultipleOfPrime += nPrime;
+                }
+            }
+            nPrime++;
+        }
+
+        System.out.println("Prime numbers up to " + nInputNum + ":");
+        int nCounter = 0;
+        int nNum = 2;
+        while (nNum <= nInputNum) {
+            if (isPrime[nNum]) {
+                System.out.println(nNum);
+                nCounter++;
+            }
+            nNum++;
+        }
+
+        // Calculate and print the percentage of primes
+        double percentage = (nCounter * 100.0) / nInputNum;
+        System.out.println("There are " + nCounter + " primes between 2 and " + nInputNum +
+                           " (" + String.format("%.0f", percentage) + "% are primes)");
     }
 }
