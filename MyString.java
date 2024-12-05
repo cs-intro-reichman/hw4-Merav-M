d908aa2f54760bbbe85524a66ca109c81e0bfbd9
Merav-M <merav.mauhnoom@post.runi.ac.il>
2024-12-05 22:50:04 +0200

    String and Char Array

--- MyString.java
+++ MyString.java
@@ -18,13 +18,42 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String sRetValue = "";
+
+        for (int i = 0; i < str.length(); i++) {
+          char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+            c = (char) (c + 32);
+            sRetValue += c;
+            }   
+            else{
+                  sRetValue += c;
+            }
+          }
+      
+          return sRetValue;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        boolean bRetValue = false;
+        int nLengthStr1 = str1.length();
+        int nLengthStr2 = str2.length();
+         int nLastIndexToCheck = nLengthStr1 - nLengthStr2 + 1;
+        
+         for(int i = 0; i < nLastIndexToCheck && !bRetValue; i++) {
+
+            boolean bIsContained = true;
+            for(int j = 0; j <nLengthStr2; j++)
+            {
+               if(str2.charAt(j) != str1.charAt(j + i)){
+                bIsContained = false;
+                break;
+               }
+            }
+            bRetValue = bIsContained;
+         }
+
+        return bRetValue;
     }
 }
